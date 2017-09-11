558e0d6e08aad76673262538994bb7b38b6bf5bf
culshoefer <christophsulshoefer@gmail.com>
2017-09-11 13:28:40 -0400

    Adjusted path of installation to current dir

--- devsetup.sh
+++ devsetup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
-
+dir=pwd
 # binutils
-cd /home/vagrant
+cd $dir
 curl -S -L -O http://www.student.cs.uwaterloo.ca/~cs350/os161_repository/os161-binutils.tar.gz
 tar -xf os161-binutils.tar.gz
 rm os161-binutils.tar.gz
 cd binutils-2.17+os161-2.0.1
-./configure --nfp --disable-werror --target=mips-harvard-os161 --prefix=/home/vagrant/sys161/tools
+./configure --nfp --disable-werror --target=mips-harvard-os161 --prefix=$dir/sys161/tools
 make
 make install
-cd /home/vagrant
+cd $dir
 mkdir -p sys161/bin
 
 #env
@@ -24,7 +24,7 @@ curl -S -L -O http://www.student.cs.uwaterloo.ca/~cs350/os161_repository/os161-g
 tar -xzf os161-gcc.tar.gz
 rm os161-gcc.tar.gz
 cd gcc-4.1.2+os161-2.0
-./configure -nfp --disable-shared --disable-threads --disable-libmudflap --disable-libssp --target=mips-harvard-os161 --prefix=/home/vagrant/sys161/tools
+./configure -nfp --disable-shared --disable-threads --disable-libmudflap --disable-libssp --target=mips-harvard-os161 --prefix=$dir/sys161/tools
 make
 make install 
 cd ..
@@ -34,7 +34,7 @@ curl -S -L -O http://www.student.cs.uwaterloo.ca/~cs350/os161_repository/os161-g
 tar -xf os161-gdb.tar.gz
 rm os161-gdb.tar.gz
 cd gdb-6.6+os161-2.0
-./configure --target=mips-harvard-os161 --prefix=/home/vagrant/sys161/tools --disable-werror 
+./configure --target=mips-harvard-os161 --prefix=$dir/sys161/tools --disable-werror 
 make
 make install
 cd ..
@@ -48,27 +48,27 @@ rm os161-bmake.tar.gz
 cd bmake
 tar -xzf ../os161-mk.tar.gz
 rm ../os161-mk.tar.gz
-./boot-strap --prefix=/home/vagrant/sys161/tools
+./boot-strap --prefix=$dir/sys161/tools
 # ./make-bootstrap.sh
 
-mkdir -p /home/vagrant/sys161/tools/bin
-cp /home/vagrant/bmake/Linux/bmake /home/vagrant/sys161/tools/bin/bmake-20101215
-rm -f /home/vagrant/sys161/tools/bin/bmake
-ln -s bmake-20101215 /home/vagrant/sys161/tools/bin/bmake
-mkdir -p /home/vagrant/sys161/tools/share/man/cat1
-cp /home/vagrant/bmake/bmake.cat1 /home/vagrant/sys161/tools/share/man/cat1/bmake.1
-sh /home/vagrant/bmake/mk/install-mk /home/vagrant/sys161/tools/share/mk
-cd /home/vagrant
+mkdir -p $dir/sys161/tools/bin
+cp $dir/bmake/Linux/bmake $dir/sys161/tools/bin/bmake-20101215
+rm -f $dir/sys161/tools/bin/bmake
+ln -s bmake-20101215 $dir/sys161/tools/bin/bmake
+mkdir -p $dir/sys161/tools/share/man/cat1
+cp $dir/bmake/bmake.cat1 $dir/sys161/tools/share/man/cat1/bmake.1
+sh $dir/bmake/mk/install-mk $dir/sys161/tools/share/mk
+cd $dir
 
 pwd
 #link
-mkdir -p /home/vagrant/sys161/bin
-cd /home/vagrant/sys161/tools/bin
-sh -c 'for i in mips-*; do ln -s /home/vagrant/sys161/tools/bin/$i /home/vagrant/sys161/bin/cs350-`echo $i | cut -d- -f4-`; done'
-if [ ! -e "/home/vagrant/sys161/bin/bmake" ]; then
-    ln -s /home/vagrant/sys161/tools/bin/bmake /home/vagrant/sys161/bin/bmake
+mkdir -p $dir/sys161/bin
+cd $dir/sys161/tools/bin
+sh -c 'for i in mips-*; do ln -s $dir/sys161/tools/bin/$i $dir/sys161/bin/cs350-`echo $i | cut -d- -f4-`; done'
+if [ ! -e "$dir/sys161/bin/bmake" ]; then
+    ln -s $dir/sys161/tools/bin/bmake $dir/sys161/bin/bmake
 fi
-cd /home/vagrant
+cd $dir
 
 
 
@@ -80,11 +80,11 @@ cd sys161-1.99.06
 ./configure --prefix=$HOME/sys161 mipseb
 make
 make install
-cd /home/vagrant/sys161
+cd $dir/sys161
 if [ ! -e "sys161.conf" ]; then
     ln -s share/examples/sys161/sys161.conf.sample sys161.conf
 fi
-cd /home/vagrant
+cd $dir
 
 
 
